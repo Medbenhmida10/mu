1623f4acc4e218c9c51ef30236990a59ee4f5539
Medbenhmida10 <61783020+Medbenhmida10@users.noreply.github.com>
2021-11-12 01:34:48 +0100

    Update mu.js

--- mu.js
+++ mu.js
@@ -244,7 +244,7 @@
                         },
                         onSelect: function (oEvent) {
                             for (var i=0 ; this.getView().byId("Tree").getSelectedItems().length ; i++){
-                                var listselected = listselected + this.getView().byId("Tree").getSelectedItems()[i].getBindingContext("Multiinput_1").getObject().text
+                                var listselected += this.getView().byId("Tree").getSelectedItems()[i].getBindingContext("Multiinput_1").getObject().text + ","
                                 listselected.show
                             }
                         },
