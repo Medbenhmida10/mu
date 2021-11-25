a41fbe72a66d10d1c544463e1c6710fbbbe7461d
Medbenhmida10 <61783020+Medbenhmida10@users.noreply.github.com>
2021-11-25 13:14:31 +0100

    Update mu.js

--- mu.js
+++ mu.js
@@ -277,7 +277,6 @@
                             for (var i = 0; i < this.getView().byId("Tree").getSelectedItems().length; i++) {
                                 listselected += this.getView().byId("Tree").getSelectedItems()[i].getBindingContext("Multiinput_1").getObject().text + ","
                             }
-new sap.m.MessageToast.show(this.getView().byId("Tree").getSelectedItems()[i].getBindingContext("Multiinput_1").getObject().id)
                             console.log(listselected);
                             _unit = listselected;
                             // new sap.m.MessageToast.show(_unit)
