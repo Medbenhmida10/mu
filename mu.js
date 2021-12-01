43ff0c56e14eb7c0e6be6f05b791a788e4360e40
Medbenhmida10 <61783020+Medbenhmida10@users.noreply.github.com>
2021-12-01 02:07:07 +0100

    Update mu.js

--- mu.js
+++ mu.js
@@ -255,10 +255,10 @@
 
                             // this.getView().setModel(oModel, that.widgetName);
                             sap.ui.getCore().setModel(oModel, "Multiinput_1");
-                            sap.ui.getCore().byId("Tree").setModel(oModel, "Multiinput_1");
-                            if (sap.ui.getCore().byId("Tree").getItems().length!=0){
-                                sap.ui.getCore().byId("Tree").expandToLevel(9999);
-                                sap.ui.getCore().byId("Tree").getItems()[3].setSelected(true);}
+                            this.getView().byId("Tree").setModel(oModel, "Multiinput_1");
+                            if (this.getView().byId("Tree").getItems().length!=0){
+                                this.getView().byId("Tree").expandToLevel(9999);
+                                this.getView().byId("Tree").getItems()[3].setSelected(true);}
                         },
                         onAfterRendering: function () {
  // set explored app's demo model on this sample
@@ -269,10 +269,10 @@
 
                             // this.getView().setModel(oModel, that.widgetName);
                             sap.ui.getCore().setModel(oModel, "Multiinput_1");
-                            sap.ui.getCore().byId("Tree").setModel(oModel, "Multiinput_1");
-                            if (sap.ui.getCore().byId("Tree").getItems().length!=0){
-                                sap.ui.getCore().byId("Tree").expandToLevel(9999);
-                                sap.ui.getCore().byId("Tree").getItems()[3].setSelected(true);}
+                            this.getView().byId("Tree").setModel(oModel, "Multiinput_1");
+                            if (this.getView().byId("Tree").getItems().length!=0){
+                                this.getView().byId("Tree").expandToLevel(9999);
+                                this.getView().byId("Tree").getItems()[3].setSelected(true);}
                             
                         },
                         onDefaultSelction  : function(event) {
