3ba6e996c2549d9c20b71125d1a6ec7a800d721c
Medbenhmida10 <61783020+Medbenhmida10@users.noreply.github.com>
2022-02-15 01:06:58 +0100

    Update mu.js

--- mu.js
+++ mu.js
@@ -276,22 +276,16 @@ toggleOpenState: function(oEvent) {
 
 
                         onDefaultSelction  : function(event) {
-                            if(time){
+                            if(time>0){
                                 if(this.byId("Tree")!=undefined){
                                     this.byId("Tree").expandToLevel(9999);
-                                    
+                                    this.getView().byId("Tree").getItems()[5].setBlocked(true);
                                     if(this.byId("Tree").getItems()[3]!=undefined){
                                         this.byId("Tree").getItems()[3].setSelected(true);
-                                        
-                                        if(this.byId("Tree").getItems()[5]!=undefined){
-                                         this.getView().byId("Tree").getItems()[5].setBlocked(true);
-                                      
-                                        time = 0; 
-                                        }
+                                        time = 0;
                                     }
-                                }
-                                
-                        }
+                                }    
+                            }
                             
                             
 $('.sapMTreeItemBaseChildren').css('background-color', 'white');
