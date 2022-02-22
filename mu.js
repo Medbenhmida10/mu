773ff32e352f84a4dfa5c0eeea17197e835825b4
Medbenhmida10 <61783020+Medbenhmida10@users.noreply.github.com>
2022-02-22 10:54:20 +0100

    Update mu.js

--- mu.js
+++ mu.js
@@ -288,9 +288,9 @@ toggleOpenState: function(oEvent) {
                             }
                     for (var j = 0; j < this.getView().byId("Tree").getItems().length; j++) {
                         if(this.getView().byId("Tree").getItems()[j].mProperties.title =='PREXIGE'){
-                           //   sap.ui.getCore().byId(this.byId("Tree").getItems()[j].$().find('.sapMCb').attr('id')).setEnabled(false);
-                           //     this.byId("Tree").getItems()[j].$().css('color', 'lightgrey');
-                            this.byId("Tree").getItems()[j].setBlocked(true);
+                         //     sap.ui.getCore().byId(this.byId("Tree").getItems()[j].$().find('.sapMCb').attr('id')).setEnabled(false);
+                           //   this.byId("Tree").getItems()[j].$().css('color', 'lightgrey');
+                            this.byId("Tree").getItems()[j].$().css('pointer-events', 'none');
                         }
                     }        
 $('.sapMTreeItemBaseChildren').css('background-color', 'white');
