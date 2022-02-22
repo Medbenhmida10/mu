36abde026b4c1219b9d170812c7f19166b528707
Medbenhmida10 <61783020+Medbenhmida10@users.noreply.github.com>
2022-02-22 10:56:42 +0100

    Update mu.js

--- mu.js
+++ mu.js
@@ -289,7 +289,7 @@ toggleOpenState: function(oEvent) {
                     for (var j = 0; j < this.getView().byId("Tree").getItems().length; j++) {
                         if(this.getView().byId("Tree").getItems()[j].mProperties.title =='PREXIGE'){
                          //     sap.ui.getCore().byId(this.byId("Tree").getItems()[j].$().find('.sapMCb').attr('id')).setEnabled(false);
-                           //   this.byId("Tree").getItems()[j].$().css('color', 'lightgrey');
+                            this.byId("Tree").getItems()[j].$().css('color', 'lightgrey');
                             this.byId("Tree").getItems()[j].$().css('pointer-events', 'none');
                         }
                     }        
